361357973965038c71d0d12616aee8f65605930f
TomKellyGenetics <tomkellygenetics@gmail.com>
2018-06-18 17:37:05 +0900

    add codecov

--- R/plot.R
+++ R/plot.R
@@ -19,8 +19,8 @@ convergence <- function(clf, show=FALSE, save=NULL, p_thresh=0.99, voter_thresh=
     
     doubs_per_run <- list()
     for(i in 1:clf$n_iters){
-      cum_p_values <- clf$all_p_values_[1:i]
-      cum_vote_average <- apply(sum_p_values_, 2, function(x){
+      cum_p_values_ <- clf$all_p_values_[1:i]
+      cum_voting_average_ <- lapply(cum_p_values_, function(x) {
         x <- ifelse(is.infinite(x), NA, x)
         mean(as.numeric(x > p_thresh), na.rm = TRUE)
       })
