9fc5e91db979ea737aefb9cc55bad131ca1d05a7
TomKellyGenetics <tomkellygenetics@gmail.com>
2019-07-18 11:51:56 +0900

    add pbmc8k data

---
+++ R/pbmc8k.R
@@ -0,0 +1,8 @@
+#' PBMC 8K data
+#'
+#' Reactome pathway R-HSA-109704 for the interactions in the phosphoinositide-3-kinase cascade
+#' 
+#' @format A sparese matrix of 33694 genes and 8381 cell barcodes
+#' @source PathwayCommons \url{"http://cf.10xgenomics.com/samples/cell-exp/2.1.0/pbmc8k/pbmc8k_filtered_gene_bc_matrices.tar.gz"}
+#' @importFrom Matrix readMM
+"pbmck8k"
